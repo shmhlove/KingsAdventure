18230425cc9feed0abef0c6cd6324242ac8a7d0a
shmhlove <shmhlove@naver.com>
2018-02-14 00:57:22 +0900

    [프레임워크] iOS FixupFile GenCode 추가 For PostProcess

---
+++ Assets/02_Script/Tools/BuildScript/Editor/SHBuildPostProcess.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+using System;
+using System.Collections.Generic;
+
+class SHBuildPostProcess
+{
+    [PostProcessBuild(999)]
+    public static void OnPostProcessBuild(BuildTarget eTarget, string strTargetPath)
+    {
+        if (eTarget == BuildTarget.iOS)
+        {
+            SHiOSFixupFiles.FixFirebaseCode(strTargetPath);
+        }
+    }
+}
