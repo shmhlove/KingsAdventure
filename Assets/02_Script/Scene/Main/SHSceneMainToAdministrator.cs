7b0bcb3d6ea9293bf1493f0837eb19c68cce8e5e
shmhlove <shmhlove@naver.com>
2018-04-10 01:59:47 +0900

    [써드파티] Firebase 다시 추가

--- Assets/02_Script/Scene/Main/SHSceneMainToAdministrator.cs
+++ Assets/02_Script/Scene/Main/SHSceneMainToAdministrator.cs
@@ -7,9 +7,9 @@ using System.IO;
 using System.Text;
 using System.Collections;
 using System.Threading;
-//using System.Threading.Tasks;
+using System.Threading.Tasks;
 
-//using Firebase.Storage;
+using Firebase.Storage;
 
 public class SHSceneMainToAdministrator : SHMonoWrapper
 {
@@ -91,42 +91,42 @@ public class SHSceneMainToAdministrator : SHMonoWrapper
 
     public void OnClickOfFBStorage_Download()
     {
-        //// 파일 다운로드
-        //FirebaseStorage pStorage = FirebaseStorage.DefaultInstance;
-    
-        //string strPlatform = SHHard.GetPlatformStringByEnum(Single.AppInfo.GetRuntimePlatform());
-        //StorageReference pRootRef = pStorage.GetReferenceFromUrl("gs://kingsadventure-development.appspot.com/");
-        //StorageReference pSceneRef = pRootRef.Child(string.Format("/{0}/AssetBundle/scene/", strPlatform));
-        //StorageReference pIntroRef = pSceneRef.Child("intro.scene");
-        
-        //Debug.LogFormat("Root Path : {0}", pRootRef.Path);
-        //Debug.LogFormat("Root Name : {0}", pRootRef.Name);
-        //Debug.LogFormat("Root Bucket : {0}", pRootRef.Bucket);
-        
-        //Debug.LogFormat("Scene Path : {0}", pSceneRef.Path);
-        //Debug.LogFormat("Scene Name : {0}", pSceneRef.Name);
-        //Debug.LogFormat("Scene Bucket : {0}", pSceneRef.Bucket);
-        
-        //Debug.LogFormat("Intro Path : {0}", pIntroRef.Path);
-        //Debug.LogFormat("Intro Name : {0}", pIntroRef.Name);
-        //Debug.LogFormat("Intro Bucket : {0}", pIntroRef.Bucket);
-        
-        //// URL로 다운로드
-        //pIntroRef.GetDownloadUrlAsync().ContinueWith((Task<Uri> pTask) =>
-        //{
-        //    Debug.Log("Done GetDownloadUrl");
-        
-        //    if ((false == pTask.IsFaulted) && (false == pTask.IsCanceled))
-        //    {
-        //        Debug.LogFormat("Download Path is {0}", pTask.Result);
-        
-        //        Single.Coroutine.WWW((pWWW) => 
-        //        {
-        //            Debug.Log("Intro Scene Bundle Download Complate");
-        //        }, WWW.LoadFromCacheOrDownload(pTask.Result.OriginalString, 0));
-        //    }
-        //});
-        
+        // 파일 다운로드
+        FirebaseStorage pStorage = FirebaseStorage.DefaultInstance;
+
+        string strPlatform = SHHard.GetPlatformStringByEnum(Single.AppInfo.GetRuntimePlatform());
+        StorageReference pRootRef = pStorage.GetReferenceFromUrl("gs://kings-adventure-22251209.appspot.com/");
+        StorageReference pSceneRef = pRootRef.Child(string.Format("/{0}/AssetBundle/scene/", strPlatform));
+        StorageReference pIntroRef = pSceneRef.Child("intro.scene");
+
+        Debug.LogFormat("Root Path : {0}", pRootRef.Path);
+        Debug.LogFormat("Root Name : {0}", pRootRef.Name);
+        Debug.LogFormat("Root Bucket : {0}", pRootRef.Bucket);
+
+        Debug.LogFormat("Scene Path : {0}", pSceneRef.Path);
+        Debug.LogFormat("Scene Name : {0}", pSceneRef.Name);
+        Debug.LogFormat("Scene Bucket : {0}", pSceneRef.Bucket);
+
+        Debug.LogFormat("Intro Path : {0}", pIntroRef.Path);
+        Debug.LogFormat("Intro Name : {0}", pIntroRef.Name);
+        Debug.LogFormat("Intro Bucket : {0}", pIntroRef.Bucket);
+
+        // URL로 다운로드
+        pIntroRef.GetDownloadUrlAsync().ContinueWith((Task<Uri> pTask) =>
+        {
+            Debug.Log("Done GetDownloadUrl");
+
+            if ((false == pTask.IsFaulted) && (false == pTask.IsCanceled))
+            {
+                Debug.LogFormat("Download Path is {0}", pTask.Result);
+
+                Single.Coroutine.WWW((pWWW) =>
+                {
+                    Debug.Log("Intro Scene Bundle Download Complate");
+                }, WWW.LoadFromCacheOrDownload(pTask.Result.OriginalString, 0));
+            }
+        });
+
         // Byte 배열로 다운로드
         //const long lMaxAllowedSize = 1 * 1024 * 1024;
         //pIntroRef.GetBytesAsync(lMaxAllowedSize).ContinueWith((Task<byte[]> pTask) =>
@@ -141,13 +141,13 @@ public class SHSceneMainToAdministrator : SHMonoWrapper
         //        Debug.Log(pTask.Exception.ToString());
         //    }
         //});
-        
+
         // 스트리밍으로 다운로드
         //pIntroRef.GetStreamAsync((pStream) =>
         //{
         //    Debug.Log("Finished downloading!");
         //}, null, CancellationToken.None);
-        
+
         // 로컬파일로 다운로드
         //string strLocalURL = "file:///local/scene/intro.scene";
         //pIntroRef.GetFileAsync(strLocalURL).ContinueWith(pTask =>
